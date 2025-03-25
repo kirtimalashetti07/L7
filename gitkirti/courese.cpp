08fadbb7f8e41ecbbf936a155cc9b4545e35636d
kirtimalashetti07 <malashettikirti@gmail.com>
2025-03-25 15:47:47 +0530

    c++ program

---
+++ gitkirti/courese.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int choice:
+		switch(choice)
+		{
+			case 1:{
+				       printf("courses is cse choosen"):}
+			case 2:{
+				       printf("courese AL/ML is choodsen"):}
+			case 3:{
+				       printf("courses robotics is choosen"):}
+			case 4:{
+				       printf("courses data science is choosen"):}
+			default:{"please enter valid choice"}
+			} exit(0):
+			printf("welcome! please press 1 for cse /n press 2 for AI/ML /n press 3 for robotics  /n press 4 for data science"):
+		        if (choice==1){
+				goto case 1:}
+}
+else if(choice==2){
+	goto case 2:}
+	else if (choice==3){
+		goto case 3:}
+		else if (choice ==4){
+			goto case 4:}
+
+}
---
+++ gitkirti/courses.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+	int  choice:
+	{
+		case 1:{
+			       printf("courses in cse"):}
+		case 2:{
+			       printf("coures AI/ML is choosen"):}
+		case 3:{
+			       printf("couse robotics is choosen"):}
+		case 4:{
+			       printf("coures datascience is choosen"):}
+		default :{"please enter valid choice"}
+	}exit(0);
+	printf("wellcome please press 1 for cse /n press 2 for AI/ML /n press 3 for robotics /n press 4 for data science ")}
+	if (choice==1){
+		goto case 1:
+}
+else if (choice ==2){
+	goto case2:}
+	else if(choice==3){
+		goto case 3:}
+		else if(choice ==4){
+			goto case 4:
+		}
+}
